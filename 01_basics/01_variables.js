17094c649b12d207dc20787d8cb2326ef39e52a5
ushiv009-byte <ushiv009@gmail.com>
2026-07-19 16:01:42 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId=144553
+let accountEmail="shiv@google.com"
+var accountpassword="12345"
+let accountState;
+accountCity="Raebareli"
+accountEmail="su@gmail.com"
+accountpassword="373798"
+accountCity="Ghaziabad"
+console.log(accountId)
+console.table([accountId,accountEmail,accountpassword,accountpassword,accountState])
