82ae79c521af85e98741115c816fe2aa4bf2e4a8
ushiv009-byte <ushiv009@gmail.com>
2026-07-19 15:59:11 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"//treat all JS code as newer version
+//alert(3+3)//we are using node js not browser
+console.log(3+3)
+console.log("Shiv")
+let name="Shiv"
+let age="19"
+let isLoggedIn=false;
+let state;
+//number==>2 to power 53
+//bigint
+//string==>""
+//boolean==>true or false
+//null==>standalone value
+//undefined
+//symbol==>unique
+//object
+console.log(typeof undefined);
+console.log(typeof null);
