68490fb55ad2fbc7a61a26838793c22c79c0b8fe
ushiv009-byte <ushiv009@gmail.com>
2026-07-21 16:01:55 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,19 @@
+let score="33abc"
+console.log(typeof score);
+console.log(typeof(score));
+let valueinnumber=Number(score);
+console.log(typeof valueinnumber);
+console.log(valueinnumber);
+//"33"=>33
+//"33abc"=>NaN
+//"true"=>1;false=>0
+let isLoggedIn=1;
+let booleanisLoggedIn=Boolean(isLoggedIn);
+console.log(booleanisLoggedIn);
+//1=>true;0=>false
+//""=>false
+//"hitesh"=>true
+let someNumber=33
+let stringnumber=String(someNumber)
+console.log(stringnumber)
+console.log(typeof stringnumber)
