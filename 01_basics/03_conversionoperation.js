cfd11ae6b5404c296fc79f2a9ddfb6bb5083959e
ushiv009-byte <ushiv009@gmail.com>
2026-07-21 20:10:02 +0000

    convandcomp

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -1,19 +1,44 @@
 let score="33abc"
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 let valueinnumber=Number(score);
-console.log(typeof valueinnumber);
-console.log(valueinnumber);
+// console.log(typeof valueinnumber);
+// console.log(valueinnumber);
 //"33"=>33
 //"33abc"=>NaN
 //"true"=>1;false=>0
 let isLoggedIn=1;
 let booleanisLoggedIn=Boolean(isLoggedIn);
-console.log(booleanisLoggedIn);
+// console.log(booleanisLoggedIn);
 //1=>true;0=>false
 //""=>false
 //"hitesh"=>true
 let someNumber=33
 let stringnumber=String(someNumber)
-console.log(stringnumber)
-console.log(typeof stringnumber)
+// console.log(stringnumber)
+// console.log(typeof stringnumber)
+//*****************************operations***************************/
+let value=3
+let negvalue=-value
+// console.log(negvalue)
+// console.log(2+2)
+// console.log(2*2)
+// console.log(2-2)
+// console.log(2**3)
+// console.log(2%3)
+let str1="hello"
+let str2=" shiv"
+let str3=str1+str2
+// console.log(str3)
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// console.log((3+4)*5%3);
+// console.log(+true)
+// console.log(+"")
+let num1,num2,num3
+num1=num2=num3=2+2
+let gamecounter=100
+++gamecounter;
+console.log(gamecounter);
