16cb5c156e3c21ef9bc6e1d3eb3983954f4652b2
ushiv009-byte <ushiv009@gmail.com>
2026-07-24 11:50:01 +0000

    datatypes

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,22 @@
+//Primitive
+//7 types:String,number,boolean,null,undefined,Symbol,BigInt
+const score=100
+const scorevalue=100.3
+const isLoggedIn=false
+const outsidetemp=null
+let useremail;
+const id=Symbol('123') 
+const anotherId=Symbol('123')
+console.log(id==anotherId)
+//const bigNumber=83453277323232823863n
+//Reference(Non-Primitive)
+//Arrays,objects,Functions
+const cricketers=["Virat Kohli","Jasprit Bumrah","AB De velliers"];
+let myObj={
+    name:"Shiv",
+    age:19,
+}
+const myFunction=function(){
+    console.log("Hello World");
+}
+console.log(typeof outsidetemp);
